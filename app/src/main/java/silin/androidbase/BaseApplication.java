61e15c3f26982e7d9e6f688d2ffeb7e6a68fcfbf
Silin Na <n1207n@gmail.com>
2016-07-04 13:04:28 -0700

    feat: Added AutoDagger2 and created AutoComponent for BaseApplication

--- app/src/main/java/silin/androidbase/BaseApplication.java
+++ app/src/main/java/silin/androidbase/BaseApplication.java
@@ -5,10 +5,23 @@ import android.app.Application;
 import com.facebook.stetho.Stetho;
 import com.squareup.leakcanary.LeakCanary;
 
+import autodagger.AutoComponent;
+import silin.androidbase.modules.ContextModule;
+import silin.androidbase.modules.EnvironmentModule;
+import silin.androidbase.modules.NetworkModule;
+import silin.androidbase.modules.SharedPrefModule;
+
 /**
  * Created on 7/4/16: AndroidBase
  */
-
+@AutoComponent(
+        modules = {
+                ContextModule.class,
+                EnvironmentModule.class,
+                NetworkModule.class,
+                SharedPrefModule.class
+        }
+)
 public class BaseApplication extends Application {
     @Override
     public void onCreate() {
