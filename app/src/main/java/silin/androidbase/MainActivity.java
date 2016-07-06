cc5ec03506ace4666b399a08a42ae69f3cee835c
Silin Na <n1207n@gmail.com>
2016-07-06 01:05:28 -0700

    refactor: Fixed the scope accesses in BaseApplication

--- app/src/main/java/silin/androidbase/MainActivity.java
+++ app/src/main/java/silin/androidbase/MainActivity.java
@@ -7,14 +7,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import autodagger.AutoInjector;
 import silin.androidbase.R;
 
+@AutoInjector(BaseApplication.class)
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        BaseApplication.sharedApplication().getComponentApplication().inject(this);
+
+        setTheme(R.style.AppTheme_NoActionBar);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
